7b63d09f183d19e313534c6fd9ef6e5d5c2ddac9
baksoli <solyi0712@gmail.com>
2020-04-01 18:14:57 +0900

    2020/04/01 algorithm_arrays01_2

--- programmersProject/src/programmersProject/Array01_KthNumber.java
+++ programmersProject/src/programmersProject/Array01_KthNumber.java
@@ -16,24 +16,31 @@ public class Array01_KthNumber {
 		for(int i=0; i<commandsCnt; i++){
 			int strCnt = commands[i][0];
 			int endCnt = commands[i][1];
-			int rtnValue = commands[i][2] - 1;
-			int arrayCnt = endCnt-strCnt+1;
-			System.out.println("arrayCnt : "+arrayCnt);
-			int[] editArrray = new int[arrayCnt];
-			int p = 0;
-			for(int j=strCnt-1; j<endCnt; j++){
-				System.out.println("array["+j+"] --> "+array[j]);
-				editArrray[p]=array[j];
-				
-				System.out.println(i+" 번째 "+p+"번" + editArrray[p]);
-				p++;
-				
-			}
-			Arrays.sort(editArrray);
-			System.out.println("return될 값 :: "+editArrray[rtnValue]);
+			int rtnValue = commands[i][2]-1;
 			
-			answer[i] = editArrray[rtnValue];
-			System.out.println("-----------------------------");
+			
+			int[] rtnArrays = Arrays.copyOfRange(array, strCnt-1, endCnt);
+			Arrays.sort(rtnArrays);
+			answer[i] = rtnArrays[rtnValue];
+			
+			
+//			int arrayCnt = endCnt-strCnt+1;
+//			System.out.println("arrayCnt : "+arrayCnt); 
+//			int[] editArrray = new int[arrayCnt];
+//			int p = 0;
+//			for(int j=strCnt-1; j<endCnt; j++){
+//				System.out.println("array["+j+"] --> "+array[j]);
+//				editArrray[p]=array[j];
+//				
+//				System.out.println(i+" 번째 "+p+"번" + editArrray[p]);
+//				p++;
+//				
+//			}
+//			Arrays.sort(editArrray);
+//			System.out.println("return될 값 :: "+editArrray[rtnValue]);
+//			
+//			answer[i] = editArrray[rtnValue];
+//			System.out.println("-----------------------------");
 		}
         
         return answer;
